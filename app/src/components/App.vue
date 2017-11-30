deaa80586bfff04f470d1c9e780a74e34597290c
iMac26 <iMac26@iMac26-no-iMac.local>
2017-11-30 19:38:12 +0900

    :cry: first commit

---
+++ app/src/components/App.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  .wrap
+    Greeting(:greeting="greeting")
+</template>
+
+<script lang="ts">
+import Greeting from './Greeting.vue';
+
+interface Data {
+  greeting: string
+}
+
+export default {
+  data: function(): Data {
+    return {
+      greeting: 'HELLO! PEACE WORLD!!!'
+    }
+  },
+  components: {
+    Greeting
+  }
+}
+</script>
+
+<style lang="sass">
+
+</style>
---
+++ app/src/index.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+// import 'whatwg-fetch';
+// impot library
+// import {
+//   TweenMax,
+//   TweenLite
+// } from 'gsap';
+// import ScrollToPlugin from 'ScrollToPlugin';
+// import EasePack from 'EasePack';
+// import modernizr from 'modernizr';
+//
+// console.log(modernizr);
+import App from './components/App.vue';
+
+import Vue from 'vue';
+
+new Vue({
+  el: '#app',
+  template: '<App />',
+  components: {
+    App
+  }
+});
